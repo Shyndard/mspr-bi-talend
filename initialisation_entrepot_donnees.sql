0f8cb542bbea58a8bb10aca7f41be0cb370f1307
Custo <shyndard@gmail.com>
2021-03-04 22:20:34 +0100

    add sql + modify job

---
+++ initialisation_entrepot_donnees.sql
@@ -0,0 +1,111 @@
+-- Création d'une table 'site' représentant les entrepots stockants les produits
+DROP TABLE IF EXISTS site;
+CREATE TABLE site (
+	id_site varchar(10) primary key,
+	numero_rue varchar(10),
+	rue varchar(150),
+	code_postal varchar(5),
+	ville varchar(50)
+);
+COMMENT ON TABLE site IS 'Représentation des entrepots';
+
+-- Création d'une table 'produit'
+DROP TABLE IF EXISTS produit;
+CREATE TABLE produit(
+	id_produit varchar(10) primary key,
+	id_categorie varchar(3),
+	nom_categorie varchar(50),
+	description varchar(250),
+	commentaire varchar(250),
+	seuil smallint
+);
+COMMENT ON TABLE produit IS 'Représentation des produits';
+
+-- Création d'une table 'temps'
+DROP TABLE IF EXISTS temps;
+CREATE TABLE temps(
+	id_temps serial primary key,
+	annee smallint,
+	mois smallint,
+	jour smallint
+);
+COMMENT ON TABLE temps IS 'Représentation du temps entre 2000 et 2099 avec une précision au jour';
+
+-- Création d'une table 'client'
+DROP TABLE IF EXISTS client;
+CREATE TABLE client(
+	id_client varchar(10) primary key,
+	nom varchar(50),
+	numero_rue varchar(10),
+	rue varchar(150),
+	code_postal varchar(5),
+	ville varchar(50),
+	telephone varchar(13),
+	email varchar(50),
+	contact varchar(50)
+);
+COMMENT ON TABLE client IS 'Représentation des clients';
+
+-- Création d'une table 'fournisseur'
+DROP TABLE IF EXISTS fournisseur;
+CREATE TABLE fournisseur(
+	id_fournisseur varchar(10) primary key,
+	nom varchar(50),
+	numero_rue varchar(10),
+	rue varchar(150),
+	code_postal varchar(5),
+	ville varchar(50),
+	telephone varchar(13),
+	email varchar(50)
+);
+COMMENT ON TABLE fournisseur IS 'Représentation des fournisseurs de produit';
+
+-- Création d'une table 'achat'
+DROP TABLE IF EXISTS achat;
+CREATE TABLE achat(
+	id_site varchar(10),
+	id_temps integer,
+	id_produit varchar(10),
+	id_fournisseur varchar(10),
+	quantite integer,
+	prix_achat float,
+	CONSTRAINT pk_achat PRIMARY KEY (id_site, id_temps, id_produit, id_fournisseur)
+);
+COMMENT ON TABLE achat IS 'Représentation des achats en fonction du site, du temps, du produit et des fournisseurs';
+
+-- Création d'une table 'vente'
+DROP TABLE IF EXISTS vente;
+CREATE TABLE vente(
+	id_site varchar(10),
+	id_temps integer,
+	id_produit varchar(10),
+	id_client varchar(10),
+	quantite integer,
+	prix_vente float,
+	CONSTRAINT pk_vente PRIMARY KEY (id_site, id_temps, id_produit, id_client)
+);
+COMMENT ON TABLE vente IS 'Représentation des ventes en fonction du site, du temps, du produit et des clients';
+
+-- Génération des données pour la table 'temps' entre le 1er janvier 2000 et le 31 décembre 2099
+do $$
+declare
+   nyear integer := 2000;
+   nmonth integer := 1;
+   nday integer := 0 ; 
+begin
+	loop 
+		exit when nyear = 2100 ; 
+		nmonth = 1;
+		loop 
+			exit when nmonth = 13 ;
+				nday = 1;
+				loop 
+					exit when nday = 32 ;
+						INSERT INTO temps (annee, mois, jour) values (nyear, nmonth, nday);
+						nday := nday + 1 ;
+				end loop;
+				nmonth := nmonth + 1 ; 
+		end loop;
+		nyear := nyear + 1 ; 
+	end loop; 
+end; $$
---
+++ modification_production.sql
@@ -0,0 +1,54 @@
+-- Modification du schéma actuel
+
+-- Création de la table entrepot qui fait le lien entre
+-- un produit, son emplacement et sa localisation
+DROP TABLE IF EXISTS entrepot;
+CREATE TABLE `entrepot` (
+  `id_entrepot` int PRIMARY KEY AUTO_INCREMENT,
+  `id_loca` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Polutation de la table entrepot avec une valeur
+INSERT INTO entrepot (id_loca) 
+SELECT id_loca FROM localisation LIMIT 1;
+
+-- Modification de la table emplacement en liant avec un entrepot 
+ALTER TABLE emplacement ADD COLUMN IF NOT EXISTS ID_ENTREPOT int;
+UPDATE emplacement SET ID_ENTREPOT = (SELECT id_entrepot FROM entrepot LIMIT 1);
+
+-- Modification de la table detail_commande en liant avec un entrepot 
+ALTER TABLE detail_commande ADD COLUMN IF NOT EXISTS ID_ENTREPOT int;
+UPDATE detail_commande SET ID_ENTREPOT = (SELECT id_entrepot FROM entrepot LIMIT 1);
+
+-- Modification de la table detail_livr_f en liant avec un entrepot 
+ALTER TABLE detail_livr_f ADD COLUMN IF NOT EXISTS ID_ENTREPOT int;
+UPDATE detail_livr_f SET ID_ENTREPOT = (SELECT id_entrepot FROM entrepot LIMIT 1);
+
+-- Liaison des données actuelles de la table emplacement
+-- avec un entrepot
+UPDATE emplacement SET ID_ENTREPOT = (SELECT id_entrepot FROM entrepot LIMIT 1);
+
+-- Création de la table entrepot qui fait le lien entre
+-- un produit, son emplacement et sa localisation
+DROP TABLE IF EXISTS entrepot_stock;
+CREATE TABLE `entrepot_stock` (
+  `code_produit` varchar(8) NOT NULL,
+  `no_emplacement` int(3) NOT NULL,
+  `quantite_stock` int(4) NOT NULL,
+  `date_stock` date NOT NULL,
+  `seuil` int(4) NOT NULL,
+  PRIMARY KEY (`code_produit`,`no_emplacement`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Polutation de la table entrepot_stock avec les données actuelles
+INSERT INTO entrepot_stock (code_produit, no_emplacement, quantite_stock, date_stock, seuil)  
+SELECT code_produit, no_emplacement, quantite_stock, date_stock, 100 as seuil
+FROM produit;
+
+-- Ajout de la colonne seuil dans produit afin de déterminer
+-- quand il y aura besoin de reapprovisionner un produit
+ALTER TABLE produit ADD COLUMN IF NOT EXISTS SEUIL int(4) default 100;
+
+-- Suppression des données en doublons apportées par la
+-- modification du schéma sql
+ALTER TABLE produit DROP IF EXISTS no_emplacement, DROP IF EXISTS quantite_stock, DROP IF EXISTS date_stock;
